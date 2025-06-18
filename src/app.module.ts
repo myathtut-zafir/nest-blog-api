615d14e7adbec1be1559fb3f7dab9eb8f277a495
myathtut-zafir <aa.htut@gmail.com>
2025-06-18 08:02:38 +0700

    feat: add user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5435,
+      username: 'mht',
+      password: '12345678',
+      database: 'nestBlog',
+      autoLoadEntities: true,
+      synchronize: true,
+      // entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
