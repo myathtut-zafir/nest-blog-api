18e796ee16f6c88738fb68d79ab2590c5178e935
myathtut-zafir <aa.htut@gmail.com>
2025-06-18 08:08:34 +0700

    fix: entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,9 +1,8 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseEntity } from 'src/common/entities/base.entity';
+import { Column, Entity } from 'typeorm';
 
 @Entity('users')
-export class User {
-  @PrimaryGeneratedColumn()
-  id: number;
+export class User extends BaseEntity {
   @Column({ type: 'varchar' })
   name: string;
   @Column({ type: 'varchar', unique: true })
