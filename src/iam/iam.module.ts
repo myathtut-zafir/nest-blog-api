23b18b5f78232763b176ebfab928ac09fd2d121b
myathtut-zafir <aa.htut@gmail.com>
2025-06-20 08:25:53 +0700

    feat: setup jwt

--- src/iam/iam.module.ts
+++ src/iam/iam.module.ts
@@ -1,4 +1,18 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
+import jwtConfig from './config/jwt.config';
+import { ConfigModule } from '@nestjs/config';
+import { HashingService } from './hashing/hashing.service';
+import { BcryptService } from './hashing/bcrypt.service';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+    ConfigModule.forFeature(jwtConfig),
+  ],
+  providers: [{ provide: HashingService, useClass: BcryptService }],
+})
 export class IamModule {}
